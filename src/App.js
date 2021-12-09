1cdd18aca857d6e08260eef9bf308737b5041b27
Vikas D <viky@Vikass-MacBook-Air.local>
2021-12-09 19:23:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SampleDiv from './test-common/components/sample'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SampleDiv value={"prop"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test-common/components/sample.jsx
@@ -0,0 +1,9 @@
+import * as react from "react";
+
+const sampleDiv = (props) => {
+    return (
+        <div> Hello {props.value}</div>
+    )
+}
+
+export default sampleDiv;
